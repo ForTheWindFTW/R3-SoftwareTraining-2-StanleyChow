ff96506065b562f061705178135b6cdbe1236ad3
Stanley Chow <51177611+ForTheWindFTW@users.noreply.github.com>
2020-11-21 17:59:28 -0500

    updated readme, added more comments and refactored

--- README.md
+++ README.md
@@ -1,5 +1,64 @@
 # R3 Software Training 2
 ## Maze Generation, TCP Commands
-## Stanley Chow
+### Stanley Chow
 
-This repo contains scmaze.py which will generate a random maze
+## Progess
+- [x] Milestone 1: create a random maze generator
+- [ ] Milestone 2: transmit PWM commands over TCP to traverse a maze
+
+## Milestone 1
+### Objective
+Using the pygame library, develop a random maze generator algorithm which is able to create mazes of variable size.
+
+### Requirements
+- Create and draw a random maze given a n by n grid
+- Use the base pygame code found [here](/snippets/base_pygame_code.py) as the basis for my code
+- The grid width and height should **not** be hardcoded
+- A good resource for maze generation algorithms can be found
+[here](http://weblog.jamisbuck.org/2011/2/7/maze-generation-algorithm-recap)
+
+### Steps
+- Installed and added the [pygame](https://www.pygame.org/docs/) library to the python interpreter
+- From the [base pygame code](/snippets/base_pygame_code.py) provided, I made a function to draw each edge of a grid
+- Researched on maze generation algorithms and used a
+[variation](https://weblog.jamisbuck.org/2011/1/10/maze-generation-prim-s-algorithm) of Prim's algorithm in
+[scmaze.py](/scmaze.py)
+
+### How it Works
+- The pre-set values WIDTH and HEIGHT are the respective dimensions of the window which pygame uses.
+- After the initialization, the window generates an initial maze that is displayed and enters the main control loop.
+This loop handles actions on the window, with the only action currently implemented is left click (mouse1). Left
+clicking will randomly generate a new maze of the same dimensions.
+- To create a maze, `scmaze.gen_prim_maze' is called, returning a graph where the edges are where there is no wall.
+- Those walls are excluded from rendering the lines while iterating through all of the edges
+
+## Milestone 2
+### Objective
+Using the Python Socket library, create a program which takes in the array of the solution path and sends a stream of
+strings. These strings will go to the Arduino, similar to training #1 and contain commands which will explain how the
+motors should move.
+
+### Requirements
+- A send and receive program using TCP
+- Use the TCP stream to send commands for the solution path to solve a maze
+- Assume that each step will take 1 second
+- Send `[0][0][0][0]` to stop the motors (in general, use strings with format `[M1_A][M1_B][M2_A][M2_B]`)
+- The path solution code will be <span style="text-decoration: underline;">provided</span>, so **only the TCP stream**
+**needs to be programmed**.
+- Also create a program that recieves the TCP packet and prints it out (to test if it works) 
+
+### Steps
+- WIP
+
+### How it Works
+- WIP
+
+## Reflection
+So far, this second software training module has allowed me to utilize my previous experience. I have previously used
+JavaFX and Swing, which allows me to feel much more comfortable with the gui drawing in the pygame library. Installing
+the pygame module was a bit confusing, as simply downloading with pip was not enough for it to work, but I found a
+solution after some online searching. I did not face many other difficulties, as I have also had previous experience
+with some graph theory algorithms, understanding how to modify Prim's Algorithm was not very difficult for me. However,
+for the second milestone, I will likely experience many more obstacles and difficulties, since I have not been able to
+grasp the concepts of networking, sockets, and packet communication.
+
--- scmaze.py
+++ scmaze.py
@@ -1,24 +1,27 @@
 import random
 
 
-def add_frontier(x, y, verts, frontier=None):
+def add_frontier(x, y, graph, frontier=None):
+    """
+    Adds the vertices adjacent to (x, y) in graph to frontier and returns it, if it is a unique element
+    """
     def add_frontier_N():
-        if 0 <= y - 1 < len(verts) and not verts[y - 1][x] and (x, y - 1) not in frontier:
+        if 0 <= y - 1 < len(graph[0]) and not graph[0][y - 1][x] and (x, y - 1) not in frontier:
             frontier.append((x, y - 1))
         return frontier
 
     def add_frontier_S():
-        if 0 <= y + 1 < len(verts) and not verts[y + 1][x] and (x, y + 1) not in frontier:
+        if 0 <= y + 1 < len(graph[0]) and not graph[0][y + 1][x] and (x, y + 1) not in frontier:
             frontier.append((x, y + 1))
         return frontier
 
     def add_frontier_W():
-        if 0 <= x - 1 < len(verts[0]) and not verts[y][x - 1] and (x - 1, y) not in frontier:
+        if 0 <= x - 1 < len(graph[0][0]) and not graph[0][y][x - 1] and (x - 1, y) not in frontier:
             frontier.append((x - 1, y))
         return frontier
 
     def add_frontier_E():
-        if 0 <= x + 1 < len(verts[0]) and not verts[y][x + 1] and (x + 1, y) not in frontier:
+        if 0 <= x + 1 < len(graph[0][0]) and not graph[0][y][x + 1] and (x + 1, y) not in frontier:
             frontier.append((x + 1, y))
         return frontier
 
@@ -33,39 +36,52 @@ def add_frontier(x, y, verts, frontier=None):
     return frontier
 
 
-def adjacent(x, y, verts):
-    return [(x + dx, y + dy) for dx, dy in zip((-1, 1, 0, 0), (0, 0, -1, 1)) if 0 <= x + dx < len(verts[0]) and 0 <= y + dy < len(verts) and verts[y + dy][x + dx]]
+def adjacent(x, y, graph):
+    """
+    Returns a list with all points adjacent to the vertex at (x, y)
+    """
+    return [(x + dx, y + dy) for dx, dy in zip((-1, 1, 0, 0), (0, 0, -1, 1)) if 0 <= x + dx < len(graph[0][0]) and 0 <= y + dy < len(graph[0])]
 
 
-def mark(x, y, verts):
-    verts[y][x] = True
-    return
+def visit(x, y, graph):
+    """
+    Sets the vertex (x, y) in graph to be visited (True)
+    """
+    graph[0][y][x] = True
 
 
 def gen_prim_maze(dim=None):
     """
     Generate a maze with a list of vertices of a graph using a variation of Prim's Algorithm
+    Assumes dim is None (size of maze is 1x1) or an int or a 2-tuple giving both width and height of a maze to generate
     """
     if dim is None:
         dim = 1
-    if type(dim) in (int, float):
+    if type(dim) is (int):
         dim = dim, dim
 
+    # Initialize (V, E) of the graph where all vertices are False
     graph = [[False] * dim[0] for _ in range(dim[1])], []
+
+    # Select a random vertex and visits it (True)
     x0, y0 = random.randrange(len(graph[0][0])), random.randrange(len(graph[0]))
-    mark(x0, y0, graph[0])  # Marks point as visited
-    frontier = add_frontier(x0, y0, graph[0])
+    visit(x0, y0, graph)
+
+    # Add the 'frontier' vertices; adjacent vertices that are not visited and not yet added
+    frontier = add_frontier(x0, y0, graph)
 
     # Choose random pt, add adjacent to possibilities
+    # Perform this to all vertices, note the -1 due to the initial vertex above
     for _ in range(dim[0] * dim[1] - 1):
         to_x, to_y = random.choice(frontier)
         frontier.remove((to_x, to_y))
 
-        mark(to_x, to_y, graph[0])
-        add_frontier(to_x, to_y, graph[0], frontier)
+        visit(to_x, to_y, graph)
+        add_frontier(to_x, to_y, graph, frontier)
 
-        sources = adjacent(to_x, to_y, graph[0])
-        from_x, from_y = sources[random.randrange(len(sources))]
+        # sources = [pos for pos in adjacent(to_x, to_y, graph) if graph[0][pos[1]][pos[0]]]
+        sources = list(filter(lambda pos: graph[0][pos[1]][pos[0]], adjacent(to_x, to_y, graph)))
+        from_x, from_y = random.choice(sources)
 
         # Edges are from left to right and downwards
         if to_x < from_x or to_y < from_y:
